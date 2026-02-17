47c017b3d604e33e7f71132075671eda3ba7884f
Ajay Kumar <ajaychimata205@gmail.com>
2026-02-17 18:03:26 +0530

    portfolio-added

--- src/data/newsletters.tsx
+++ src/data/newsletters.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import portfolio from "../assets/images/logo/portfolio/wisecap-profile-january-13-2026.pdf"
+// import portfolio from "../assets/images/logo/portfolio/wisecap-profile-january-13-2026.pdf"
 
-
+import portfolio from "../assets/images/logo/portfolio/Wisecap-Portfolio.pdf"
 
 
 export const newsletters = [
